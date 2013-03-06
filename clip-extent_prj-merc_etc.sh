71e503a5de174d2641ebf73aecfa6a17eea814c6
Chris Henrick <chrishenrick@gmail.com>
2013-03-06 10:04:48 -0800

    added grep-find-replace.sh and updated clip-extent_prj-merc_etc.sh

--- clip-extent_prj-merc_etc.sh
+++ clip-extent_prj-merc_etc.sh
@@ -1,36 +1,58 @@
-#!/bin/bash
+#*************************************************************
+# OGR2OGR -clipscr project 
+# this script clips all data in a directory to a bounding box coordinates, projects clipped data to a desired SRC, \
+# moves processed data to a new location, and removes unprojected clipped data.
+# I find this useful for cartography when working with Natural Earth Data, see http://www.naturalearthdata.com/
+#*************************************************************
 
-#clip all shapefiles to specified lat lon bounding coordinates (x min y min x max y max)
+
+!/bin/bash
+
+# clip all shapefiles to specified lat lon bounding coordinates (x min y min x max y max)
+# note: data must be in WGS84, if in a different projection use that SRS' coordinate values 
+#*************************************************************
+
 for FILE in *.shp
 do
  echo "Transforming $FILE file..."
  FILENEW=`echo $FILE | sed "s/.shp/_clip.shp/"`
  ogr2ogr \
- -clipsrc -179.9 42 -115 77\
+ -wrapdateline \
+ -clipsrc --27.4 8 66.3 49.2 \
  $FILENEW  $FILE
 
 done
 
-#project all clipped data to...
+# project all clipped data to a desired SRS such as EPSG:26904
+#*************************************************************
+# frequently used projections:
+# 3395 wgs84 world mercator
+# 900913 google web-mercator
+# 3857 pseudo mercator (depreciated)
+
 for FILE in *_clip.shp
 do
  echo "Transforming $FILE file..."
  FILENEW=`echo $FILE | sed "s/.shp/_3338.shp/"`
  ogr2ogr \
--s_srs "EPSG:4326" -t_srs "EPSG:3338"   \
-$FILENEW  $FILE\
+-s_srs "EPSG:4326" -t_srs "EPSG: "\
+ $FILENEW  $FILE\
 
 done
 
-#move all clipped and projected files to a temp folder
+#move all clipped and projected files to the project folder.
+#*************************************************************
+
 for FILE in *_3338.*
 do
  echo "moving $FILE file..."
- mv $FILE /path/to/folder/
+ mv $FILE /Volumes/Avalon/Mercator/Cartography/Handbooks/Anchorage_Denali_01/data/shp/natural-earth_50m_processed/ 
  
 done
 
-#delete all clipped files still in wgs84 that won't be needed.
+#delete all clipped, unprojected files still in WGS84 (original coordinate system) that won't be needed.
+#*************************************************************
+
 for FILE in *_clip.*
 do
  echo "deleting $FILE file..."
---
+++ grep-find-replace.sh
@@ -0,0 +1,26 @@
+# *****************************************************************************************
+# find_and_replace_in_files.sh
+# This script does a recursive, case sensitive directory search and replace of files
+# To make a case insensitive search replace, use the -i switch in the grep call
+# uses a startdirectory parameter so that you can run it outside of specified directory - else this script will modify itself!
+# *****************************************************************************************
+
+!/bin/bash
+# **************** Change Variables Here ************
+startdirectory="~/temp/chrishenrick"
+searchterm="src="place-holder.png""
+replaceterm="src="images/place-holder.png""
+# **********************************************************
+
+echo "******************************************"
+echo "* Search and Replace in Files Version .1 *"
+echo "******************************************"
+
+        for file in $(grep -l -R $searchterm $startdirectory)
+          do
+           sed -e "s/$searchterm/$replaceterm/ig" $file > /tmp/tempfile.tmp
+           mv /tmp/tempfile.tmp $file
+           echo "Modified: " $file
+        done
+
+echo " *** Yay! All Done! *** "
