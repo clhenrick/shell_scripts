ddfb3a582821441d0bf864e22c52e034fe212457
Chris Henrick <chrishenrick@gmail.com>
2013-03-06 15:26:22 -0800

    Update clip-extent_prj-merc_etc.sh

--- clip-extent_prj-merc_etc.sh
+++ clip-extent_prj-merc_etc.sh
@@ -2,11 +2,14 @@
 
 # *************************************************************
 # OGR2OGR -clipscr project 
-# this script clips all data in a directory to a bounding box coordinates, projects clipped data to a desired SRC, \
+# this script clips all data in a directory to a bounding box coordinates, projects clipped data to a desired SRS, \
 # moves processed data to a new location, and removes unprojected clipped data.
 # I find this useful for cartography when working with Natural Earth Data, see http://www.naturalearthdata.com/
 # *************************************************************
 
+# To do: make variables for the lat lon coordinates and for -s_srs and -t_srs
+# this way the user can change the parameters in one place rather than throughout the script.
+# *************************************************************
 
 # clip all shapefiles to specified lat lon bounding coordinates (x min y min x max y max)
 # note: data must be in WGS84, if in a different projection use that SRS' coordinate values 
@@ -14,7 +17,7 @@
 
 for FILE in *.shp
 do
- echo "Clipping $FILE file..."
+ echo "clipping $FILE file..."
  FILENEW=`echo $FILE | sed "s/.shp/_clip.shp/"`
  ogr2ogr \
  -wrapdateline \
@@ -23,9 +26,9 @@ do
 
 done
 
-# project all clipped data to a desired SRS such as EPSG:26904
+# project all clipped data to a desired SRS such as EPSG:900913
 # *************************************************************
-# frequently used projections:
+# frequently used EPSG codes for projecting spatial data:
 # 3395 wgs84 world mercator
 # 900913 google web-mercator
 # 3857 pseudo mercator (depreciated)
@@ -33,7 +36,7 @@ done
 #project all clipped data to a desired SRS such as EPSG:3395 WGS84 World Mercator
 for FILE in *_clip.shp
 do
- echo "Projecting $FILE file..."
+ echo "projecting $FILE file..."
  FILENEW=`echo $FILE | sed "s/.shp/_3395.shp/"`
  ogr2ogr \
 -s_srs "EPSG:4326" -t_srs "EPSG:3395"\
@@ -51,7 +54,7 @@ do
 
 done
 
-#delete all clipped, unprojected files still in WGS84 (original coordinate system) that won't be needed.
+#delete all clipped, unprojected files in WGS84 (original coordinate system) that won't be needed.
 #*************************************************************
 
 for FILE in *_clip.*
