0ffdc017973b3a52d8fdc23a89fcff23f0342914
Chris Henrick <chrishenrick@gmail.com>
2013-03-18 12:26:17 -0700

    added gpx-to-shp.sh

---
+++ gpx-to-shp.sh
@@ -0,0 +1,59 @@
+# this script will clean up gpx data for rendering in illustrator with mapublisher.
+# separate gpx data into one shapefile for lines, a shapefile for waypoints, and a shapefile for track points,
+# and project data to a desired CRS
+# ************************************************************************************************************
+#!/bin/bash
+
+# script variables assigned as follows:
+OUTDR='/Users/chrishenrick/Projects/TAH_Phoenix_01/00_DATA/shp_gpx_processed/'  # ouput directory here
+TSRS='EPSG:2223'  # target CRS EPSG code here
+PRJ=`echo $TSRS | sed s/\"//g | cut -f2 -d ':'`   # variable values for sed here:
+
+# extract tracks and rename file
+for FILE in *.gpx
+do
+ echo "converting $FILE file's tracks..."
+ FILENEW=`echo $FILE | sed "s/.gpx/_tracks_$PRJ.shp/"`
+ ogr2ogr \
+ -f "ESRI Shapefile" \
+ -t_srs $TSRS \
+ $FILENEW $FILE \
+ tracks
+
+done
+
+# extract waypoints and rename file
+for FILE in *.gpx
+do
+ echo "converting $FILE file's waypoints..."
+ FILENEW=`echo $FILE | sed "s/.gpx/_waypoints_$PRJ.shp/"`
+ ogr2ogr \
+ -f "ESRI Shapefile" \
+ -t_srs $TSRS \
+ $FILENEW $FILE \
+ waypoints
+ 
+done
+
+# extract track points and rename file
+for FILE in *.gpx
+do
+ echo "converting $FILE file's waypoints..."
+ FILENEW=`echo $FILE | sed "s/.gpx/_track-points_$PRJ.shp/"`
+ ogr2ogr \
+ -f "ESRI Shapefile" \
+ -t_srs $TSRS \
+ $FILENEW $FILE \
+ track_points
+ 
+done
+
+# move files into an output directory
+for FILE in *$PRJ*
+do
+ echo "moving $FILE to $OUTDR..."
+ mv $FILE $OUTDR
+
+done
+ 
+exit
