ce5e34ae98b4dcb58d96fb8aea68279bba5217b2
Landon Reed <landontreed@gmail.com>
2013-11-06 17:27:42 -0500

    built out basic for loop

---
+++ geojson-to-shp.sh
@@ -0,0 +1,15 @@
+# this script will export all .geojson files in a directory to .shp files in a specified subdirectory 
+# ************************************************************************************************************
+#!/bin/bash
+
+NEWDIR="data/"
+mkdir $NEWDIR # creates new subdirectory
+for FILE in *.geojson # cycles through all files in directory (case-sensitive!)
+do
+	echo "converting file: $FILE..."
+	FILENEW=`echo $FILE | sed "s/.geojson/_new.shp/"` # replaces old filename
+	ogr2ogr \
+	-f "ESRI Shapefile" \
+	$NEWDIR$FILENEW $FILE
+done
+exit
