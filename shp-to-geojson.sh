64660f6b6a28cc16f68ee51c6280ddd7f77e82a1
Landon Reed <landontreed@gmail.com>
2013-11-12 18:12:08 -0500

    added reprojection

--- shp-to-geojson.sh
+++ shp-to-geojson.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
 # this script will export all .shp files in a directory to .geojson files in a specified subdirectory 
 # ************************************************************************************************************
-#!/bin/bash
 
 NEWDIR=${2:-"$1/geojson"}
 for FILE in ${1:-}*.shp # cycles through all files in directory (case-sensitive!)
 do
-	if [stat -c %s $FILE > 30000000]
+	if [wc -c $FILE > 30000000]
 	then
 		echo "Skipping $FILE because it's pretty big and GitHub might complain!"
 		continue
@@ -14,6 +14,7 @@ do
 	FILENEW=`echo | basename $FILE | sed "s/.shp/_new.geojson/"` # replaces old filename
 	ogr2ogr \
 	-f "GeoJSON" \
+	-t_srs "urn:ogc:def:crs:OGC:1.3:CRS84" \
 	$NEWDIR$FILENEW $FILE
 done
 exit
