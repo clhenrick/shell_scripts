ba13b25a8c0517581a38b49d259747d841741f0a
Landon Reed <landontreed@gmail.com>
2013-11-12 17:31:15 -0500

    added file size flag

--- shp-to-geojson.sh
+++ shp-to-geojson.sh
@@ -5,7 +5,12 @@
 NEWDIR=${2:-"$1/geojson"}
 for FILE in ${1:-}*.shp # cycles through all files in directory (case-sensitive!)
 do
-	echo "converting file: $FILE..."
+	if [stat -c %s $FILE > 30000000]
+	then
+		echo "Skipping $FILE because it's pretty big and GitHub might complain!"
+		continue
+	fi
+	echo "converting file: $FILE...into $NEWDIR$FILENEW..."
 	FILENEW=`echo | basename $FILE | sed "s/.shp/_new.geojson/"` # replaces old filename
 	ogr2ogr \
 	-f "GeoJSON" \
