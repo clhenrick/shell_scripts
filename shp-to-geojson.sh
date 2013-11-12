cad389323e3f2f4df720ab67463bbf50723ede5b
Landon Reed <landontreed@gmail.com>
2013-11-12 18:51:30 -0500

    finally fixed the size check... #syntaxproblems

--- shp-to-geojson.sh
+++ shp-to-geojson.sh
@@ -5,14 +5,15 @@
 NEWDIR=${2:-"$1/geojson"}
 for FILE in ${1:-}*.shp # cycles through all files in directory (case-sensitive!)
 do
-	SIZE=$(wc -c $FILE)
-	if [$SIZE -gt 30000000]
+	SIZE=$(stat -c%s "$FILE")
+	echo "$SIZE bytes"
+	if [ "$SIZE" -gt "30000000" ]
 	then
-		echo "Skipping $FILE because it's pretty big and GitHub might complain!"
+		echo "WARNING: Skipping $FILE because it's pretty big and GitHub might complain!"
 		continue
 	fi
-	echo "converting file: $FILE...into $NEWDIR$FILENEW..."
 	FILENEW=`echo | basename $FILE | sed "s/.shp/_new.geojson/"` # replaces old filename
+	echo "converting file: $FILE...into $FILENEW..."
 	ogr2ogr \
 	-f "GeoJSON" \
 	-t_srs "urn:ogc:def:crs:OGC:1.3:CRS84" \
