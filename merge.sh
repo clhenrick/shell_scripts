16cb867b1255fc12c2cd2b65c3c4e9c43deb1b95
Chris Henrick <chrishenrick@gmail.com>
2014-12-31 11:39:30 -0500

    update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
-for FILE in *.shp
+# Merges a directory of shapefiles into one.
+# caveats: shapefiles do not support multiple geometry types, 
+#   so all shapefiles must be of the same geometry.
+
+FILE='nl_merged.shp' # name of file to be merged to
+LAYER='nl_merged' # must be same as above but without `.shp` extension
+SSRS='EPSG:2227' # source CRS
+TSRS='EPSG:2227' # target CRS
+
+for i in $(ls *.shp)
 do
- echo "Transforming $FILE file..."
- ogr2ogr \
- -update -append merge.shp $FILE -f "ESRI Shapefile" -nln merge
 
+    if [ -f "$FILE" ]
+    then
+            echo "transforming and merging $i..."
+            ogr2ogr \
+            -f 'ESRI Shapefile' \
+            -s_srs $SSRS \
+            -t_srs $TSRS \
+            -update -append $FILE $i \
+            -nln $LAYER
+    else
+            echo "creating $FILE..."
+            ogr2ogr \
+            -f 'ESRI Shapefile' \
+            -s_srs $SSRS \
+            -t_srs $TSRS \
+            $FILE $i
+fi
 done
-
-#note: change the -s_srs parameters to the projection of the data you're working with.
-#ex: 4326 = WGS84 and 900913 = Google Web Mercator
